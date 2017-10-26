4b3b3d8aadfd833a687bf5aa7877c92aa6e0aa6e
Bene <benedikt.wiberg@gmail.com>
2017-10-26 20:21:34 +0200

    First working version

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 
 using namespace cv;
 
-const String PATH_LEFT = "Q:\\Libraries\\Stereo\\BlockMatcher\\img\\Aloe\\view1.png";
-const String PATH_RIGHT = "Q:\\Libraries\\Stereo\\BlockMatcher\\img\\Aloe\\view5.png";
+const String PATH_LEFT = "C:\\Projects\\Stereo\\BlockMatcher\\img\\Aloe\\view1.png";
+const String PATH_RIGHT = "C:\\Projects\\Stereo\\BlockMatcher\\img\\Aloe\\view5.png";
 
 int main(int argc, char** argv)
 {
@@ -34,30 +34,45 @@ int main(int argc, char** argv)
 	namedWindow(window_left, WINDOW_AUTOSIZE);
 	namedWindow(window_right, WINDOW_AUTOSIZE);
 	namedWindow(window_result, WINDOW_AUTOSIZE);
+	
 
-
-	uint block_size = 25;
-	uint max_offset = 100;
-	Mat result = Mat(img_left.rows, img_left.cols, CV_8U);
+	int block_size = 11;
+	uint max_offset = 80;
+	Mat result = Mat(img_left.rows, img_left.cols, CV_8S);
+	Mat best_fit = Mat(img_left.rows, img_left.cols, CV_8U);
+	best_fit.setTo(255);
 	Mat intermediate;
 	cvtColor(img_left, img_left, CV_BGR2GRAY);
 	cvtColor(img_right, img_right, CV_BGR2GRAY);
-	for (int i = 0; i <= max_offset; i++)
+
+	std::cout << img_left.type();
+	 
+	for (int ioff = 0; ioff <= max_offset*2; ++ioff)
 	{
-		int offset = max_offset - i;
-		subtract(img_left(Range(0, img_left.rows), Range(offset, img_left.cols)),
-					img_right(Range(0, img_left.rows), Range(0, img_right.cols - offset)),
-					intermediate, noArray(), CV_8S);
-		imshow(window_result, intermediate);
-		waitKey(0);
-		std::cout << intermediate.at<uchar>(0,0) << " ";
-		abs(intermediate);
-		std::cout << intermediate.at<uchar>(0, 0) << "\n";
-		imshow(window_result, intermediate);
-		waitKey(0);
-		//blur(intermediate, intermediate, Size(block_size, block_size), Point(-1,-1), BORDER_DEFAULT);
-		imshow(window_result, intermediate);
-		waitKey(0);
+		int i = ioff - max_offset;
+		int left_start =	i < 0 ? -i : 0;
+		int left_end =		i < 0 ? img_left.cols : img_left.cols - i;
+		int right_start =	i < 0 ? 0 : i;
+		int right_end =		i < 0 ? img_right.cols + i : img_right.cols;
+		absdiff(img_right(Range(0, img_left.rows), Range(right_start, right_end)), img_left(Range(0, img_left.rows), Range(left_start, left_end)), intermediate);
+
+		imshow(window_left, intermediate);
+		blur(intermediate, intermediate, Size(block_size, block_size));
+
+		for (size_t y = 0; y < img_left.rows; y++)
+		{
+			for (size_t x = 0; x < right_end && x < left_end; x++)
+			{
+				if (intermediate.at<uchar>(y, x) < best_fit.at<uchar>(y, left_start + x)) {
+					best_fit.at<uchar>(y, left_start + x) = intermediate.at<uchar>(y, x);
+					result.at<char>(y, left_start + x) = i;
+				}
+			}
+		}
+
+		imshow(window_right, img_right(Range(0, img_left.rows), Range(right_start, right_end)));
+		imshow(window_result, result);
+ 		waitKey(5);
 	}
 	
 	
